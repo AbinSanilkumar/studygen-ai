129a453966079db9a780ebbcc3bc253be312f177
Abin <abi903719@gmail.com>
2026-05-16 16:08:00 +0530

    feat: added Note model and admin integration

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Note(models.Model):
+
+    image = models.ImageField(upload_to='notes/')
+
+    extracted_text = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Note {self.id}"
